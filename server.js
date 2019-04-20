d2a831112447eb6c7bf196bcbc0feb1f2a9232a1
Gabriel Bishop <gabe@Gabriels-MacBook-Pro.local>
2019-04-20 15:59:30 -0400

    register login logout

--- server.js
+++ server.js
@@ -28,8 +28,43 @@ app.use(function (req, res, next) {
 
 const commentsService = require('./services/comments.service.server');
 const dbService = require('./services/db.service.server');
+const profileService = require('./services/profile.service.server');
+
 
 commentsService(app);
 dbService(app);
+profileService(app);
+
+app.get('/api/session/set/:name/:value',
+  setSession);
+app.get('/api/session/get/:name',
+  getSession);
+app.get('/api/session/get',
+  getSessionAll);
+app.get('/api/session/reset',
+  resetSession);
+
+
+function setSession(req, res) {
+  var name = req.params['name'];
+  var value = req.params['value'];
+  req.session[name] = value;
+  res.send(req.session);
+}
+
+function getSession(req, res) {
+  var name = req.params['name'];
+  var value = req.session[name];
+  res.send(value);
+}
+
+function getSessionAll(req, res) {
+  res.send(req.session);
+}
+
+function resetSession(req, res) {
+  req.session.destroy();
+  res.send(200);
+}
 
 app.listen(process.env.PORT || 5000);
