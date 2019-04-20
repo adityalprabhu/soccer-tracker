d6d67819297262496d3a09d8976670a0cfab5ea3
Gabriel Bishop <gabe@Gabriels-MacBook-Pro.local>
2019-04-19 22:00:42 -0400

    don't worry, fixed it for real

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {Router} from '@angular/router';
 import {TeamService} from '../../services/teamService/team.service';
 
 @Component({
@@ -39,11 +39,11 @@ export class TeamComponent implements OnInit {
     });
   }
 
-  findTeamDetails(tid){
+  findTeamDetails(tid) {
 
     this.team = {
-      name : "",
-      logo: ""
+      name: '',
+      logo: ''
     };
 
     this.teamService.findTeamDetails(tid).subscribe(res => {
@@ -59,7 +59,7 @@ export class TeamComponent implements OnInit {
     this.teamService.findTeamFixtures(teamId).subscribe(res => {
       this.allMatches = res;
 
-      if (res['api'].fixtures != {}){
+      if (res['api'].fixtures != {}) {
         this.leagueId = res['api'].fixtures[Object.keys(res['api'].fixtures)[0]].league_id;
       }
 
@@ -67,13 +67,12 @@ export class TeamComponent implements OnInit {
       this.upcomingMatches = [];
 
       let today = new Date();
-      console.log(this.allMatches)
+      console.log(this.allMatches);
       const entries = Object.entries(this.allMatches.api.fixtures);
-      for (let [x,y] of entries){
-        if(today < new Date(y['event']['date'])){
+      for (let [x, y] of entries) {
+        if (today < new Date(y['event_date'])) {
           this.upcomingMatches.push(y);
-        }
-        else {
+        } else {
           this.pastMatches.push(y);
         }
       }
@@ -3199,171 +3198,171 @@ export class TeamComponent implements OnInit {
 
   }
 
-  findRoster(){
+  findRoster() {
     this.roster = {
-      "api": {
-        "results": 37,
-        "coachs": [
-          "Unai Emery"
+      'api': {
+        'results': 37,
+        'coachs': [
+          'Unai Emery'
         ],
-        "players": [
+        'players': [
           {
-            "number": "19",
-            "player": "Bernd Leno"
+            'number': '19',
+            'player': 'Bernd Leno'
           },
           {
-            "number": "5",
-            "player": "Sokratis Papastathopoulos"
+            'number': '5',
+            'player': 'Sokratis Papastathopoulos'
           },
           {
-            "number": "16",
-            "player": "Rob Holding"
+            'number': '16',
+            'player': 'Rob Holding'
           },
           {
-            "number": "20",
-            "player": "Shkodran Mustafi"
+            'number': '20',
+            'player': 'Shkodran Mustafi'
           },
           {
-            "number": "2",
-            "player": "Hector Bellerin"
+            'number': '2',
+            'player': 'Hector Bellerin'
           },
           {
-            "number": "7",
-            "player": "Henrikh Mkhitaryan"
+            'number': '7',
+            'player': 'Henrikh Mkhitaryan'
           },
           {
-            "number": "11",
-            "player": "Lucas Torreira"
+            'number': '11',
+            'player': 'Lucas Torreira'
           },
           {
-            "number": "17",
-            "player": "Alex Iwobi"
+            'number': '17',
+            'player': 'Alex Iwobi'
           },
           {
-            "number": "31",
-            "player": "Sead Kolasinac"
+            'number': '31',
+            'player': 'Sead Kolasinac'
           },
           {
-            "number": "34",
-            "player": "Granit Xhaka"
+            'number': '34',
+            'player': 'Granit Xhaka'
           },
           {
-            "number": "14",
-            "player": "Pierre-emerick Aubameyang"
+            'number': '14',
+            'player': 'Pierre-emerick Aubameyang'
           },
           {
-            "number": "1",
-            "player": "Petr Cech"
+            'number': '1',
+            'player': 'Petr Cech'
           },
           {
-            "number": "4",
-            "player": "Mohamed El Nenny"
+            'number': '4',
+            'player': 'Mohamed El Nenny'
           },
           {
-            "number": "8",
-            "player": "Aaron Ramsey"
+            'number': '8',
+            'player': 'Aaron Ramsey'
           },
           {
-            "number": "10",
-            "player": "Mesut Ozil"
+            'number': '10',
+            'player': 'Mesut Ozil'
           },
           {
-            "number": "15",
-            "player": "Ainsley Maitland-niles"
+            'number': '15',
+            'player': 'Ainsley Maitland-niles'
           },
           {
-            "number": "29",
-            "player": "Matteo Guendouzi Olie"
+            'number': '29',
+            'player': 'Matteo Guendouzi Olie'
           },
           {
-            "number": "49",
-            "player": "Edward Nketiah"
+            'number': '49',
+            'player': 'Edward Nketiah'
           },
           {
-            "number": "",
-            "player": "Alexandre Lacazette"
+            'number': '',
+            'player': 'Alexandre Lacazette'
           },
           {
-            "number": "",
-            "player": "Stephan Lichtsteiner"
+            'number': '',
+            'player': 'Stephan Lichtsteiner'
           },
           {
-            "number": "",
-            "player": "Ainsley Maitland-Niles"
+            'number': '',
+            'player': 'Ainsley Maitland-Niles'
           },
           {
-            "number": "",
-            "player": "Laurent Koscielny"
+            'number': '',
+            'player': 'Laurent Koscielny'
           },
           {
-            "number": "",
-            "player": "Nacho Monreal"
+            'number': '',
+            'player': 'Nacho Monreal'
           },
           {
-            "number": "",
-            "player": "Bukayo Saka"
+            'number': '',
+            'player': 'Bukayo Saka'
           },
           {
-            "number": "69",
-            "player": "Joseph Willock"
+            'number': '69',
+            'player': 'Joseph Willock'
           },
           {
-            "number": "25",
-            "player": "Carl Jenkinson"
+            'number': '25',
+            'player': 'Carl Jenkinson'
           },
           {
-            "number": "27",
-            "player": "Konstantinos Mavropanos"
+            'number': '27',
+            'player': 'Konstantinos Mavropanos'
           },
           {
-            "number": "",
-            "player": "Denis Suarez"
+            'number': '',
+            'player': 'Denis Suarez'
           },
           {
-            "number": "55",
-            "player": "Emile Smith-rowe"
+            'number': '55',
+            'player': 'Emile Smith-rowe'
           },
           {
-            "number": "26",
-            "player": "Emiliano Martinez"
+            'number': '26',
+            'player': 'Emiliano Martinez'
           },
           {
-            "number": "47",
-            "player": "Charlie Gilmour"
+            'number': '47',
+            'player': 'Charlie Gilmour'
           },
           {
-            "number": "47",
-            "player": "Zech Medley"
+            'number': '47',
+            'player': 'Zech Medley'
           },
           {
-            "number": "3",
-            "player": "Jordi Osei-tutu"
+            'number': '3',
+            'player': 'Jordi Osei-tutu'
           },
           {
-            "number": "72",
-            "player": "Tyreece John-jules"
+            'number': '72',
+            'player': 'Tyreece John-jules'
           },
           {
-            "number": "",
-            "player": "Dejan Iliev"
+            'number': '',
+            'player': 'Dejan Iliev'
           },
           {
-            "number": "",
-            "player": "Tyreece John-Jules"
+            'number': '',
+            'player': 'Tyreece John-Jules'
           }
         ]
       }
-    }
+    };
   }
 
-  findTeamLeagueStanding(leagueId){
+  findTeamLeagueStanding(leagueId) {
     this.teamService.findLeagueStandings(leagueId).subscribe(res => {
       let leagueStandings = res['api'].standings[0];
 
-      for (let team of leagueStandings){
-        if (team.team_id == this.teamId){
+      for (let team of leagueStandings) {
+        if (team.team_id == this.teamId) {
           this.team['leagueStanding'] = team.rank;
-          console.log(this.team['leagueStanding'])
+          console.log(this.team['leagueStanding']);
         }
       }
     });
@@ -3372,10 +3371,10 @@ export class TeamComponent implements OnInit {
   findLiveFixtures() {
     this.teamService.findLiveFixtures().subscribe(res => {
 
-      let allLiveFixtures =  Object.values(res['api'].fixtures);
+      let allLiveFixtures = Object.values(res['api'].fixtures);
 
-      for (let fixture of allLiveFixtures){
-        if (fixture['homeTeam_id'] == this.teamId || fixture['awayTeam_id'] == this.teamId){
+      for (let fixture of allLiveFixtures) {
+        if (fixture['homeTeam_id'] == this.teamId || fixture['awayTeam_id'] == this.teamId) {
           this.liveFixture = fixture;
           console.log(this.liveFixture);
         }
