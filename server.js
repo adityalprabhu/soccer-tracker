36cad7f275f4c697677c624a8094de96eab4b599
Aditya Prabhu <adityalprabhu@gmail.com>
2019-04-24 01:40:28 -0400

    server

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static('./dist/soccer-tracker'));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin",
-    "http://localhost:4200");
+    "*");
   res.header("Access-Control-Allow-Headers",
              "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods",
