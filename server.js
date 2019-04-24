dbefea27a2b29517606aa95aac3f14fe5b235ef5
Aditya Prabhu <adityalprabhu@gmail.com>
2019-04-24 12:47:43 -0400

    heroku link change

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(function (req, res, next) {
   // res.header("Access-Control-Allow-Origin",
   //   "http://localhost:4200");
   res.header("Access-Control-Allow-Origin",
-    "*");
+    "https://soccer-tracker.herokuapp.com");
   res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods",
